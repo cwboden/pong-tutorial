834c83e9c591417ec3624612331c493e6383c044
Carson Boden <cwboden@umich.edu>
2020-12-12 21:29:50 -0800

    Pong Tutorial: Lesson 3

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 //! Pong Tutorial 1
 
 mod pong;
+mod systems;
 
 use amethyst::{
     prelude::*,
     core::transform::TransformBundle,
+    input::{InputBundle, StringBindings},
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
         types::DefaultBackend,
@@ -19,6 +21,10 @@ fn main() -> amethyst::Result<()> {
 
     let app_root = application_root_dir()?;
     let display_config_path = app_root.join("config").join("display.ron");
+    let binding_path = app_root.join("config").join("bindings.ron");
+
+    let input_bundle = InputBundle::<StringBindings>::new()
+        .with_bindings_from_file(binding_path)?;
 
     let game_data = GameDataBuilder::default()
         .with_bundle(
@@ -29,7 +35,9 @@ fn main() -> amethyst::Result<()> {
                 )
                 .with_plugin(RenderFlat2D::default()),
         )?
-        .with_bundle(TransformBundle::new())?;
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(input_bundle)?
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"]);
 
     let assets_dir = app_root.join("assets");
     let mut game = Application::new(assets_dir, Pong, game_data)?;
--- src/pong.rs
+++ src/pong.rs
@@ -17,8 +17,6 @@ impl SimpleState for Pong {
 
         let sprite_sheet_handle = load_sprite_sheet(world);
 
-        world.register::<Paddle>();
-
         initialize_paddles(world, sprite_sheet_handle);
         initialize_camera(world);
     }
